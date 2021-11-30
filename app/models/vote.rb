83c4605f2a541a5d4c17c156d81167ccf88a8111
pulgamecanica <pulgamecanica11@gmail.com>
2021-11-30 22:10:59 +0000

    Api42 User exists?

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,27 @@
 class Vote < ApplicationRecord
   belongs_to :puzzle_contender
+  validates_presence_of :username
+  validate :valid_user_42api
+
+  private
+
+    def valid_user_42api
+      user = username.downcase.strip
+      client = OAuth2::Client.new(Rails.application.credentials.api42_uid, Rails.application.credentials.api42_secret, site: "https://api.intra.42.fr")
+      token = client.client_credentials.get_token
+      puts ">>>" * 10 + "---" * 20 + "<<<" * 10
+      begin
+        response = token.get("/v2/users/" + user)
+        puts "Response: (should be 200): " + response.status.to_s
+        puts "New Vote registered"
+        puts "Login: " + response.parsed["login"]
+        puts "Campus: " + response.parsed["campus"].first["country"]
+        puts "Puzzle: " + self.puzzle_contender.puzzle.title +  " | Last Votes Count: " + self.puzzle_contender.votes.count.to_s
+      rescue
+        puts "something went wrong when check if #{user} exists..."
+        puts response
+        errors.add(:username, "Not a valid username __REASON__")
+      end
+      puts ">>>" * 10 + "---" * 20 + "<<<" * 10
+    end
 end
